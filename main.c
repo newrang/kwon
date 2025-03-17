7ec600cf21ddd1ef3e229a70fd13416f5754cf33
minhyeok <133848892+newrang@users.noreply.github.com>
2025-03-17 11:29:46 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello world!\n");
+
+    return 0;
+}
+ //#include <stdio.h>
+
+ //int main() {
+    //input(printf{"dog를 입력하세요 : \n"})
+
+    //return 0;
+ //}
